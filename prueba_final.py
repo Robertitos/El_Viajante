eda73e865848bcf1528679639a1e54c46bef8137
Robert Raicea <robyrubi_96@hotmail.es>
2022-03-25 13:47:23 +0100

    rapidfoi

--- prueba_final.py
+++ prueba_final.py
@@ -8,44 +8,57 @@ viajes = []
 distancias = [
     10,
     22,
-    68
+    68,
+    10,
+    22,
+    68    
 ]
 
-
 def imprime_variantes(indice):
-    contador = 0
-    for i in indice:
-        indice[contador] = ciudades[contador]
-        contador += 1
-    # -----------------------------------------------------------
-
     def swap(lst, a, b):
         lst[a], lst[b] = lst[b], lst[a]
 
     def permutar(indice, ultimo_numero, ultima_posible):
         if ultimo_numero == ultima_posible:
-            print(indice)
-
+            viajes.append(indice.copy())
         else:
             for numero_actual in range(ultimo_numero, ultima_posible + 1):
                 swap(indice, numero_actual, ultimo_numero)
                 permutar(indice, ultimo_numero + 1, ultima_posible)
                 swap(indice, numero_actual, ultimo_numero)
 
-    n = len(indice)
-    l = indice
-    permutar(l, 0, n-1)
-    return indice
+    permutar(indice, 0, len(indice) - 1)
 
-
-def imprime_distancias(buffer, distancias):
-
-    i = 0
-    max = len(distancias)
-    while i < max:
-        buffer[i] += f'distancia[{distancias[i]}]'
+def imprime_viajes(ciudades, viajes):
+    buffer = []
+    for viaje in viajes:
+        buffer_linea = ""
+        for indice in viaje:
+            buffer_linea += ' >> ' + ciudades[indice] 
+        buffer.append(buffer_linea)
 
     return buffer
 
+def imprime_distancias(buffer, distancias):
+    if(len(buffer) != len(distancias)):
+        exit("ERROR: las distanciasn mnas,dnas,dna,s")
+    
+    contador = 0
+    while contador <= len(buffer) - 1:
+        buffer[contador] += f"  DIST: [{distancias[contador]}] unidades"
+        contador += 1
+    
+    return buffer
+        
+    
 
-print(imprime_variantes(indice))
+    
+
+imprime_variantes(indice)
+buffer = imprime_viajes(ciudades, viajes)
+buffer = imprime_distancias(buffer, distancias)
+
+
+
+for linea in buffer:
+    print(linea)
